9f090419e851c25eff350e2bca00b04402dad8dc
elcolin <elise.col8@gmail.com>
2025-12-29 01:01:03 -0800

    refactor: moved printIPHeader in utils

--- src/utils.c
+++ src/utils.c
@@ -5,6 +5,21 @@ inline long get_elapsed_microseconds(struct timeval start, struct timeval end)
     return (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
 }
 
+inline void printIPHeader(struct iphdr *reply)
+{
+    if (reply == NULL)
+    {
+        printf("* ");
+        return;
+    }
+
+    uint32_t saddr = reply->saddr; // in network byte order
+    struct in_addr addr;
+    addr.s_addr = saddr;
+
+    char *ip_str = inet_ntoa(addr);
+    printf("%s  ", ip_str);
+}
 
 void triggerErrorNoFreeingIf(bool condition, char *msg, char *reason)
 {
