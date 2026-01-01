9a3dbe85a205b0ae989fa4d217288a820736ceee
elcolin <elise.col8@gmail.com>
2026-01-01 03:54:44 -0800

    chore: changed display on program launch

--- src/main.c
+++ src/main.c
@@ -7,11 +7,11 @@ int main(int argc, char *argv[])
     struct sockaddr_in  addrs[2] = {0};
     struct timeval      requestTimestamp[MAX_HOPS * NUMBER_OF_PROBES] = {};
     struct timeval      replyTimestamp[MAX_HOPS * NUMBER_OF_PROBES] = {};
-
+    
     srand(time(NULL));
     if (argc < 2 || argc > 2)
         return 1;//TO DO add more detailed error
-    printf("%s %s\n", argv[0], argv[1]);
+    printf("traceroute to %s\n", argv[1]);
     setDestinationAddress(&addrs[DESTINATION], argv[1]);
     setSourceAddress(&addrs[SOURCE], addrs[DESTINATION].sin_family);
 
