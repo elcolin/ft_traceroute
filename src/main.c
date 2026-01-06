b9cf3031f061c7f0cc09bf4fa3ab7a4a327c2053
elcolin <elise.col8@gmail.com>
2026-01-06 16:56:21 +0100

    chore: moved variables

--- src/main.c
+++ src/main.c
@@ -65,8 +65,8 @@ int main(int argc, char *argv[])
     struct sockaddr_in  addrs[2] = {0};
     struct timeval      requestTimestamp[MAX_HOPS * NUMBER_OF_PROBES] = {};
     struct timeval      replyTimestamp[MAX_HOPS * NUMBER_OF_PROBES] = {};
-    int packetLen = 0;
-    const int minPacketLen = (int) (sizeof(struct iphdr) + sizeof(struct udphdr));
+    int                 packetLen = 0;
+    const int           minPacketLen = (int) (sizeof(struct iphdr) + sizeof(struct udphdr));
 
     srand(time(NULL));
     if (argc < 2)
@@ -75,10 +75,9 @@ int main(int argc, char *argv[])
 
     if (argc > 3)
         fprintf(stderr, "Extra arg `%s' (position 3, argc 3)", argv[3]);
-
     if(setDestinationAddress(&addrs[DESTINATION], argv[1]) == FAILURE)
         errorArgumentHandler(1, argv[1]);
-    if (argc >= 3)
+    if (argc == 3)
         packetLen = setPacketLen(argv[2]);
     packetLen = packetLen > minPacketLen ? packetLen : minPacketLen;
 
