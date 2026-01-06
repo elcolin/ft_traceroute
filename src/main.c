fe6803d9b446184c4ea5ce4c97f1a888b4b4728b
elcolin <elise.col8@gmail.com>
2026-01-06 15:29:09 +0100

    feat: packetSize retrieval in main

--- src/main.c
+++ src/main.c
@@ -1,10 +1,30 @@
 #include "ft_traceroute.h"
 
-void argumentHandler(int pos, char *arg)
+#include <ctype.h>
+
+void errorArgumentHandler(int pos, char *arg)
 {
     static const char *errorStr[] = {"host", "packetlen"};
     fprintf(stderr, "Cannot handle \"%s\" cmdline arg `%s` on position %d (argc %d)\n", errorStr[pos - 1], arg, pos, pos);
     exit(EXIT_FAILURE);
+}
+typedef enum {
+    TOOBIG = -2,
+    WRONGARG = -3
+} packetLenError;
+
+int setPacketLen(char *arg)
+{
+    int res = 0;
+    for (size_t i = 0; i < strlen(arg); i++)
+    {
+        if(!isdigit(arg[i]))
+            return WRONGARG;
+    }
+    res = atoi(arg);
+    if (res >= BUFFER_SIZE)
+        return TOOBIG;
+    return res;
 }
 
 int main(int argc, char *argv[])
@@ -14,14 +34,31 @@ int main(int argc, char *argv[])
     struct sockaddr_in  addrs[2] = {0};
     struct timeval      requestTimestamp[MAX_HOPS * NUMBER_OF_PROBES] = {};
     struct timeval      replyTimestamp[MAX_HOPS * NUMBER_OF_PROBES] = {};
-    
+    int packetLen = 0;
+
     srand(time(NULL));
-    if (argc < 2 || argc > 2)
+    if (argc > 3)
+        fprintf(stderr, "Extra arg `%s' (position 3, argc 3)", argv[3]);
+    if (argc < 2 || argc > 3)
         return 1;//TO DO add more detailed error
     if(setDestinationAddress(&addrs[DESTINATION], argv[1]) == FAILURE)
-        argumentHandler(1, argv[1]);
+        errorArgumentHandler(1, argv[1]);
+    if (argc == 3)
+        packetLen = setPacketLen(argv[2]);
+    switch (packetLen) {
+        case TOOBIG:
+            fprintf(stderr, "too big packetlen %s specified\n", argv[2]);
+            exit(EXIT_FAILURE);
+        break;
+        case WRONGARG:
+            errorArgumentHandler(2, argv[2]);
+        break;
+        default:
+            packetLen = (packetLen >= (int) (sizeof(struct iphdr) + sizeof(struct udphdr)) ? packetLen : (int) (sizeof(struct iphdr) + sizeof(struct udphdr)));
+        break;
+    }
     setSourceAddress(&addrs[SOURCE], addrs[DESTINATION].sin_family);
-    printf("traceroute to %s (), %d hops max, byte packets\n", argv[1], MAX_HOPS);
+    printf("traceroute to %s (%s), %d hops max, %d byte packets\n", argv[1], inet_ntoa(addrs[DESTINATION].sin_addr), MAX_HOPS, packetLen);
 
     sockfd = initSocketFd();
     sendProbesToDestination(sockfd, addrs, requestTimestamp);
